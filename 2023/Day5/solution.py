1d45d8028beeab6e4c4aa2f5bdff51a9d1afa7bd
Alyssa Levicky <Alyssa.Levicky@hyland.com>
2023-12-20 13:44:57 -0500

    Day 5 - Two Stars

--- 2023/Day5/solution.py
+++ 2023/Day5/solution.py
@@ -1,3 +1,16 @@
+import sys
+import math
+
+def apply_rule(toApply, nextNumber):
+    for rule in toApply:
+            source = int(rule[sourceIndex])
+            range = int(rule[rangeIndex])
+            destination = int(rule[destinationIndex])
+            if source <= nextNumber <= (source + range):
+                diff = nextNumber - source
+                return destination + diff
+    return nextNumber
+
 seeds = []
 toSoil = []
 toFertilizer = []
@@ -26,13 +39,13 @@ with open("2023\Day5\input.txt") as f:
         values = [i.strip() for i in line.split(' ')]
         if len(values) == 2:
             continue
-
+        
         match mapCount:
             case 1:
                 #Delete line prefix
                 values.pop(0)
-
                 seeds = values
+               
             case 2:
                 toSoil.append(values)
             case 3:
@@ -49,85 +62,114 @@ with open("2023\Day5\input.txt") as f:
                 toLocation.append(values)
                 
 #Now Lets Do something Crazy with this
-for seed in seeds:
-    print("Seed:", seed)
+while len(seeds) > 0:
+    first = int(seeds.pop(0))
+    second = int(seeds.pop(0))
     
-    nextNumber = int(seed)
+    rangePossible = sys.maxsize
+    
+    index = 0
+    while(index <= second):
+        print("Seed:", index+first)
+        
+        nextNumber = int(index+first)
 
-    for soil in toSoil:
-        source = int(soil[sourceIndex])
-        range = int(soil[rangeIndex])
-        destination = int(soil[destinationIndex])
-        if source <= nextNumber <= (source + range):
-            diff = nextNumber - source
-            nextNumber = destination + diff
-            #print("Soil: ", nextNumber)
-            break
+        for soil in toSoil:
+            source = int(soil[sourceIndex])
+            range = int(soil[rangeIndex])
+            destination = int(soil[destinationIndex])
+            if source <= nextNumber <= (source + range):
+                diff = nextNumber - source
+                remainingRange = source + range - nextNumber
+                if remainingRange < rangePossible:
+                    rangePossible = remainingRange
+                nextNumber = destination + diff
+                break
 
-    for fertilizer in toFertilizer:
-        source = int(fertilizer[sourceIndex])
-        range = int(fertilizer[rangeIndex])
-        destination = int(fertilizer[destinationIndex])
-        if source <= nextNumber <= (source + range):
-            diff = nextNumber - source
-            nextNumber = destination + diff
-            #print("Fertilizer: ", nextNumber)
-            break
+        for fert in toFertilizer:
+            source = int(fert[sourceIndex])
+            range = int(fert[rangeIndex])
+            destination = int(fert[destinationIndex])
+            if source <= nextNumber <= (source + range):
+                diff = nextNumber - source
+                remainingRange = source + range - nextNumber
+                if remainingRange < rangePossible:
+                    rangePossible = remainingRange
+                nextNumber = destination + diff
+                break
+       
+        for water in toWater:
+            source = int(water[sourceIndex])
+            range = int(water[rangeIndex])
+            destination = int(water[destinationIndex])
+            if source <= nextNumber <= (source + range):
+                diff = nextNumber - source
+                remainingRange = source + range - nextNumber
+                if remainingRange < rangePossible:
+                    rangePossible = remainingRange
+                nextNumber = destination + diff
+                break
+        
+        for light in toLight:
+            source = int(light[sourceIndex])
+            range = int(light[rangeIndex])
+            destination = int(light[destinationIndex])
+            if source <= nextNumber <= (source + range):
+                diff = nextNumber - source
+                remainingRange = source + range - nextNumber
+                if remainingRange < rangePossible:
+                    rangePossible = remainingRange
+                nextNumber = destination + diff   
+                break     
+        
+        for temp in toTemperature:
+            source = int(temp[sourceIndex])
+            range = int(temp[rangeIndex])
+            destination = int(temp[destinationIndex])
+            if source <= nextNumber <= (source + range):
+                diff = nextNumber - source
+                remainingRange = source + range - nextNumber
+                if remainingRange < rangePossible:
+                    rangePossible = remainingRange
+                nextNumber = destination + diff   
+                break 
+        
+        for hum in toHumidity:
+            source = int(hum[sourceIndex])
+            range = int(hum[rangeIndex])
+            destination = int(hum[destinationIndex])
+            if source <= nextNumber <= (source + range):
+                diff = nextNumber - source
+                remainingRange = source + range - nextNumber
+                if remainingRange < rangePossible:
+                    rangePossible = remainingRange
+                nextNumber = destination + diff
+                break
+        
+        for loc in toLocation:
+            source = int(loc[sourceIndex])
+            range = int(loc[rangeIndex])
+            destination = int(loc[destinationIndex])
+            if source <= nextNumber <= (source + range):
+                diff = nextNumber - source
+                remainingRange = source + range - nextNumber
+                if remainingRange < rangePossible:
+                    rangePossible = remainingRange
+                nextNumber = destination + diff
+                break
+        
+        
 
-    for water in toWater:
-        source = int(water[sourceIndex])
-        range = int(water[rangeIndex])
-        destination = int(water[destinationIndex])
-        if source <= nextNumber <= (source + range):
-            diff = nextNumber - source
-            nextNumber = destination + diff
-            #print("Water: ", nextNumber)
-            break
+        print("Location: ", nextNumber)
+        if nextNumber < value or value == 0:
+            value = nextNumber
 
-    for light in toLight:
-        source = int(light[sourceIndex])
-        range = int(light[rangeIndex])
-        destination = int(light[destinationIndex])
-        if source <= nextNumber <= (source + range):
-            diff = nextNumber - source
-            nextNumber = destination + diff
-            #print("Light: ", nextNumber)
-            break
-
-    for temp in toTemperature:
-        source = int(temp[sourceIndex])
-        range = int(temp[rangeIndex])
-        destination = int(temp[destinationIndex])
-        if source <= nextNumber <= (source + range):
-            diff = nextNumber - source
-            nextNumber = destination + diff
-            #print("Temperature: ", nextNumber)
-            break
-
-    for humidity in toHumidity:
-        source = int(humidity[sourceIndex])
-        range = int(humidity[rangeIndex])
-        destination = int(humidity[destinationIndex])
-        if source <= nextNumber <= (source + range):
-            diff = nextNumber - source
-            nextNumber = destination + diff
-            #print("Humidity: ", nextNumber)
-            break
-
-    for loc in toLocation:
-        source = int(loc[sourceIndex])
-        range = int(loc[rangeIndex])
-        destination = int(loc[destinationIndex])
-        if source <= nextNumber <= (source + range):
-            diff = nextNumber - source
-            nextNumber = destination + diff
-            #print("Location: ", nextNumber)
-            break
-
-    print("Location: ", nextNumber)
-    if nextNumber < value or value == 0:
-        value = nextNumber
+        if rangePossible <= 1:
+            index += 1
+            rangePossible = sys.maxsize
+        else:
+            index += rangePossible
     
 
-
+# ONE OFF ERROR FOR PART 2.  The value was one less than what this program outputs
 print("Value: ", value)
